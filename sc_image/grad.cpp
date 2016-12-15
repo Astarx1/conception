c458b33728f4f89f06485688e1abdfe7add82ef1
Romain Amuat <romain.amuat@gmail.com>
2016-12-15 15:51:06 +0100

    Fonction sqrt speciale

--- sc_image/grad.cpp
+++ sc_image/grad.cpp
@@ -1,6 +1,6 @@
 #include "grad.h"
 
-void grad::gradDerive()
+/*void grad::gradDerive()
 {
 	// reset
 	if (reset.read() == 0)
@@ -23,6 +23,54 @@ void grad::gradDerive()
 			bdh = tmph;
 
 			pixel_out.write(sqrt(gv*gv+gh*gh)); 
+			up.at(cur_x) = pixel_in.read();
+
+			if (cur_x == nx) { 
+				cur_x = 1;
+			}
+			else {
+				cur_x++;
+			}
+		}
+	}
+
+	// On laisse passer les controles
+	bk_out = blank.read();
+	hpix_out = clk.read(); 
+}*/
+int max (int a, int b, int c) {
+	if (a >= b && a >= c)
+		return a;
+	else if (b >= a && b >= c) 
+		return b;
+	else 
+		return c;
+}
+int sqrt_ (int a, int b) {
+	return max (abs (a), abs (b), 0.75*(abs(a) + abs(b)));
+}
+void grad::gradDerive() {
+	// reset
+	if (reset.read() == 0)
+	{
+		pixel_out.write(0);
+	}
+	else if (clk.event() && clk.read()==1)
+	{	
+		if(blank.read() == 1)
+		{	
+			//std::cout << cur_x << " ";
+			tmpv = abs((int) up.at(cur_x) - (int) pixel_in.read());
+			int gv = abs((int)bdv+(int)tmpv);
+			if (gv > 255) gv = 255;
+			bdv = tmpv;
+
+			tmph = (int) up.at(cur_x) + (int) pixel_in.read();
+			int gh = abs((int)bdh-(int)tmph);
+			//if (gh > 255) gh = 255;
+			bdh = tmph;
+
+			pixel_out.write(sqrt_(gv,gh)); 
 			up.at(cur_x) = pixel_in.read();
 
 			if (cur_x == nx) { 
